7baf68ede1bdca979616a7ee3567d79573db3a99
Ambar <ambar@plotwise.com>
2020-12-07 16:07:10 +0100

    new file to test functions and various iterators

---
+++ udacity_practice/func_test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <numeric> // needed by accumulate
+#include <vector>
+using std::accumulate;
+using std::cout;
+using std::vector;
+
+int MySimpleTwoAdder(const int &i, const int &j) { return i + j; }
+
+int MyIteratorReferenceAdder(const vector<int> &myvec) {
+  int sum = 0;
+  for (auto i = myvec.begin(); i != myvec.end(); i++) // ++i also works the same
+  {
+    sum += *i;
+    cout << *i << " = whats in *i\n";
+
+    //   cout << " = whats in i" << i << "\n";
+    // errors since there is not such thing as i (operand unsuitable)
+  }
+  return sum;
+}
+
+int main() {
+  auto a = 23;
+  auto b = 42;
+  //   auto a = "23";
+  //   auto b = "42";
+  cout << "MySimpleTwoAdder output = " << MySimpleTwoAdder(a, b) << "\n";
+
+  vector<int> v{87, 23, 65, 87, 87};
+
+  cout << "MyIteratorReferenceAdder output = \n"
+       << MyIteratorReferenceAdder(v) << "\n";
+
+    cout << "Adding vector using accumulate :" <<accumulate(v.begin(), v.end(), 0) << "\n";
+}
