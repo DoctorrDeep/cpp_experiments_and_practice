5013e357f52aade68d1670c300af104bebeff2fd
Ambar <ambar@plotwise.com>
2020-12-07 16:06:38 +0100

    fix + uncomment nested for loops

--- udacity_practice/vector_test.cpp
+++ udacity_practice/vector_test.cpp
@@ -5,6 +5,14 @@ using std::cout;
 using std::vector;
 
 int main() {
+  auto k_1 = 5;
+  auto k_2 = {1, 2, 3};
+
+  cout << k_1 << "\n";
+
+  for (auto i : k_2)
+    cout << i << " ";
+  cout << "\n";
 
   vector<int> v_1{1, 2, 3};
   vector<int> v_11;
@@ -23,23 +31,14 @@ int main() {
 
   vector<vector<int>> v_2{{11, 22}, {33, 44}};
 
-//   for (auto i : v_1) {
-//     for (auto j : i) {
-//       cout << j << " ";
-//     }
-//     cout << "\n";
-//   }
-//   cout << "\n";
+  for (auto i : v_2) {
+    for (auto j : i) {
+      cout << j << " ";
+    }
+    cout << "\n";
+  }
+  cout << "\n";
 
   cout << "We vectored!"
        << " In 2D.\n";
-
-  auto k_1 = 5;
-  auto k_2 = {1, 2, 3};
-
-  cout << k_1 << "\n";
-
-  for (auto i : k_2)
-    cout << i << " ";
-  cout << "\n";
 }
