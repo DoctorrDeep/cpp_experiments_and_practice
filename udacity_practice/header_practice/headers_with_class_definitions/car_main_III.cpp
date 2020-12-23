6895e8045c220b82d8563ceb82291f02710e828b
Ambar <ambar@plotwise.com>
2020-12-23 16:31:02 +0100

    better formatting and  using dot +arrow operator

--- udacity_practice/header_practice/headers_with_class_definitions/car_main_III.cpp
+++ udacity_practice/header_practice/headers_with_class_definitions/car_main_III.cpp
@@ -7,26 +7,25 @@ using std::cout;
 using std::string;
 using std::vector;
 
-int main(){
-    vector<Sedan*> sed_vect;
-    Sedan* sed = nullptr;
+int main() {
+  vector<Sedan *> sed_vect;
+  Sedan *sed = nullptr;
 
-    vector<string> colors = {"mahogany", "velvet pink", "royal blue", "jet black"};
-    vector<int> door_count = {2,4,5};
+  vector<string> colors = {"mahogany", "velvet pink", "royal blue",
+                           "jet black"};
+  vector<int> door_count = {2, 4, 5};
 
-    for(int i{40}; i <= 60; i++){
-        sed = new Sedan(colors[i%4], i, i*7, door_count[i%3]);
-        sed_vect.push_back(sed);
-    }
+  for (int i{40}; i <= 60; i++) {
+    sed = new Sedan(colors[i % 4], i, i * 7, door_count[i % 3]);
+    sed_vect.push_back(sed);
+  }
 
-    for(Sedan* a_sedan_pt : sed_vect){
-        a_sedan_pt->IncrementDistance();
-        a_sedan_pt->IncrementDistance();
-        a_sedan_pt->IncrementDistance();
+  for (Sedan *a_sedan_pt : sed_vect) {
+    a_sedan_pt->IncrementDistance();
+    (*a_sedan_pt).IncrementDistance(); // Does the same thing as ->
+    a_sedan_pt->IncrementDistance();
 
-        a_sedan_pt->PrintCarData();
-        a_sedan_pt->PrintDoorData();
-    }
-
-
+    a_sedan_pt->PrintCarData();
+    a_sedan_pt->PrintDoorData();
+  }
 }
