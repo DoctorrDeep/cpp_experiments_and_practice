4424a2979644f7683fe8e7a23576c0bbbb364e83
Ambar <ambar@plotwise.com>
2020-12-23 16:09:23 +0100

    minor improvements to see diff btwn string and int

--- udacity_practice/astar_practice_2.cpp
+++ udacity_practice/astar_practice_2.cpp
@@ -8,6 +8,16 @@ using std::istringstream;
 using std::string;
 using std::vector;
 
+void BoardPrinter(const vector<vector<int>> &board) {
+  cout << "The board as interpreted in vectored-int form\n";
+  for (const auto &a_row : board) {
+    for (const auto &an_item : a_row) {
+      cout << an_item << " ";
+    }
+    cout << "\n";
+  }
+}
+
 vector<int> ReadNumbersFromLine(const string &line) {
   istringstream my_stream(line);
   int n;
@@ -25,6 +35,7 @@ vector<int> ReadNumbersFromLine(const string &line) {
 vector<vector<int>> ReadBoardFile(const string &filePath,
                                   const string &fileName) {
   vector<vector<int>> board;
+  vector<string> basic_board;
   std::ifstream my_file;
   string fileLocation = filePath + "/" + fileName;
   my_file.open(filePath + "/" + fileName);
@@ -34,11 +45,20 @@ vector<vector<int>> ReadBoardFile(const string &filePath,
     while (getline(my_file, line)) {
       cout << line << "\n";
       board.push_back(ReadNumbersFromLine(line));
+      basic_board.push_back(line);
     }
   } else {
     cout << fileLocation << " not found.\n";
   }
+
+  cout << "\nThe board as seen in the file.\n";
+  for (int i = 0; i < basic_board.size(); i++) {
+    cout << basic_board[i] << "\n";
+  }
   return board;
 }
 
-int main() { vector<vector<int>> the_board = ReadBoardFile(".", "1.board"); }
+int main() {
+  vector<vector<int>> the_board = ReadBoardFile(".", "1.board");
+  BoardPrinter(the_board);
+}
