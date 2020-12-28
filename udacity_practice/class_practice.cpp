4f07da5801bcfe5489353cb02ca4d26bc6d0695d
Ambar <ambar@plotwise.com>
2020-12-28 21:23:23 +0100

    class accessor mutator practice

---
+++ udacity_practice/class_practice.cpp
@@ -0,0 +1,38 @@
+#include <cassert>
+#include <iostream>
+
+class Date {
+public:
+  int Day() { return day_; }
+  void Day(int day) {
+    bool is_good_day = day > 0 && day < 32;
+    day_ = day * is_good_day + 1 * (not is_good_day);
+  }
+  int Month() { return month_; }
+  void Month(int month) {
+    bool is_good_month = month > 0 && month < 13;
+    month_ = month * is_good_month + 1 * (not is_good_month);
+  }
+  int Year() { return year_; }
+  void Year(int year) {
+    bool is_good_year = year > 0;
+    year_ = year * is_good_year + 2020 * (not is_good_year);
+  }
+
+private:
+  int day_{1};
+  int month_{1};
+  int year_{2020};
+};
+
+int main() {
+  Date date;
+  date.Day(-1);
+  date.Month(14);
+  date.Year(2000);
+  assert(date.Day() != -1);
+  assert(date.Month() != 14);
+  assert(date.Year() == 2000);
+
+  std::cout << date.Day() << "/" << date.Month() << "/" << date.Year() << "\n";
+};
