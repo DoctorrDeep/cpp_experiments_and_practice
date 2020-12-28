6e6a38ca866c26e24d1c9924186d460352df1f0f
Ambar <ambar@plotwise.com>
2020-12-28 17:03:44 +0100

    more struct practice

--- udacity_practice/struct_practice.cpp
+++ udacity_practice/struct_practice.cpp
@@ -10,12 +10,30 @@ struct Date {
 
 struct StrictDate {
 public:
-  int Day() { return day; }
-  void Day(int day) { this->day = day; }
+  int Day() {
+    return day; // get call that returns the current value of 'day' which is
+                // private
+  }
+  void Day(int day) {
+    this->day = day; // set call that sets the value of 'day' which is private
+                     // and return nothing
+  }
   std::string Month() { return month; }
   void Month(std::string month) { this->month = month; }
   int Year() { return year; }
-  void Year(int year) { this->year = year; }
+  void Year(int year) {
+    // In this case the invariant (setter) is going to check value
+    bool is_good_year = year / 1000 > 1; // check if year is after 2000 AD
+
+    // if (is_good_year) {
+    //   this->year = year;
+    // } else {
+    //   this->year = 2020;
+    // }
+
+    // the following  line is doing the same as the if condition above
+    this->year = year * is_good_year + 2020 * (not is_good_year);
+  }
 
 private:
   int day{1};
@@ -31,15 +49,16 @@ int main() {
   assert(date_one.day == 10);
   assert(date_one.month == "January");
   assert(date_one.year == 2000);
-  std::cout << "The simple struct date was checked to be " << date_one.day << "/"
-            << date_one.month << "/" << date_one.year << "\n";
+  std::cout << "The simple struct date was checked to be " << date_one.day
+            << "/" << date_one.month << "/" << date_one.year << "\n";
 
   // Example 2
   StrictDate date_two;
-  date_two.Year(1981);
+  date_two.Year(1981); // makes a set call on STruct date_two
+  // All assert are making get calls on the struct "date_two.___()"
   assert(date_two.Day() == 1);
   assert(date_two.Month() == "January");
   assert(date_two.Year() == 1981);
-  std::cout << "The accessor mutator date was checked to be " << date_two.Day() << "/"
-            << date_two.Month() << "/" << date_two.Year() << "\n";
+  std::cout << "The accessor mutator date was checked to be " << date_two.Day()
+            << "/" << date_two.Month() << "/" << date_two.Year() << "\n";
 }
