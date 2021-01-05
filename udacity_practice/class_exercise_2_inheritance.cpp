16df505646abcaa03504ed3ba15405e247c209a6
Ambar <ambar@calendar42.com>
2021-01-05 11:54:29 +0100

    better comments

--- udacity_practice/class_exercise_2_inheritance.cpp
+++ udacity_practice/class_exercise_2_inheritance.cpp
@@ -9,26 +9,32 @@ public:
   string maker = "placeholder generic";
 
   void Print() const {
-    std::cout << "This " << color << " vehicle from "<< maker << " has " << wheels << " wheels!\n";
+    std::cout << "This " << color << " vehicle from " << maker << " has "
+              << wheels << " wheels!\n";
   }
 };
 
 class Car : public Vehicle {
 public:
   bool sunroof = false;
-  string maker = "Peugeot";
+  string maker =
+      "Peugeot"; // This value will not be set automatically. CAlling/setting it
+                 // explicitly in main() will also not work! probably because
+                 // the base class value is being overwritten but base class
+                 // Print() is a const function. Use of a constructor could fix
+                 // this issue, probably
 };
 
 class Bicycle : public Vehicle {
 public:
   bool kickstand = true;
-  string maker = "Hero";
 };
 
 class Trolley : public Vehicle {
 public:
   bool handle = true;
-  string maker = "AmazonBasics";
+  // string maker = "AmazonBasics"; // See "Peugeot" above for car. The ONLY way
+  // to make it work is by NOT mentioning the "maker"
 };
 
 int main() {
@@ -42,5 +48,8 @@ int main() {
   Trolley trolley;
   trolley.wheels = 6;
   trolley.color = "Grey";
+  trolley.Print();
+  trolley.maker = "Amazon Soooo   Basic"; // This is when "maker" is actually
+                                          // set to the non-inherited value
   trolley.Print();
 };
