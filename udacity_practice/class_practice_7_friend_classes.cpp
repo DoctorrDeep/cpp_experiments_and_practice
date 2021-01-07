6521ff3c852da8993e514a1b04763bb7d4074a62
Ambar <ambar@calendar42.com>
2021-01-07 21:32:41 +0100

    friend classes practice

--- udacity_practice/class_practice_7_friend_classes.cpp
+++ udacity_practice/class_practice_7_friend_classes.cpp
@@ -1,42 +1,45 @@
-
-    // Declare a class Rectangle.
-    // Define a class Square.
-    // Add class Rectangle as a friend of the class Square.
-    // Add a private attribute side to class Square.
-    // Create a public constructor in class Square that initializes the side attribute.
-    // Add private members width and height to class Rectangle.
-    // Add a Rectangle() constructor that takes a Square as an argument.
-    // Add an Area() function to class Rectangle.
-
 // Example solution for Rectangle and Square friend classes
 #include <assert.h>
 
-// Declare class Rectangle
-class Rectangle{};
-
 // Define class Square as friend of Rectangle
-class Square{
-  private:
-  friend class Rectangle;
-}
-// Add public constructor to Square, initialize side
-    // Add friend class Rectangle
+class Square {
+public:
+  Square(const int &a_side);
 
-    // Add private attribute side
+private:
+  // Add friend class Rectangle
+  friend class Rectangle;
+  // Add private attribute side
+  const int side_;
+};
+
+// Declare class Rectangle
+class Rectangle {
+public:
+  Rectangle(const Square &square);
+  // Add public function to Rectangle: Area()
+  int Area() const;
+
+private:
+  // Add private attributes width, height;
+  const int width_;
+  const int height_;
+};
+
+// Add public constructor to Square, initialize side
+Square::Square(const int &s) : side_(s) {}
 
 // Define class Rectangle
-    // Add public function to Rectangle: Area()
-    
-    // Add private attributes width, height;
 
 // Define a Rectangle constructor that takes a Square
+Rectangle::Rectangle(const Square &sq) : width_(sq.side_), height_(sq.side_) {}
 
 // Define Area() to compute area of Rectangle
+int Rectangle::Area() const { return width_ * height_; }
 
 // Update main() to pass the tests
-int main()
-{
-    Square square(4);
-    Rectangle rectangle(square);
-    assert(rectangle.Area() == 16); 
+int main() {
+  Square square(4);
+  Rectangle rectangle(square);
+  assert(rectangle.Area() == 16);
 }
