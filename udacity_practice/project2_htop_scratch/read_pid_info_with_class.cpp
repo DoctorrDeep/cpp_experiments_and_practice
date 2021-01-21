9604cb11891b5ce82e836b4b44db7754d7e4c5a9
Ambar <ambar@calendar42.com>
2021-01-21 22:38:39 +0100

    working version of single htop printout

--- udacity_practice/project2_htop_scratch/read_pid_info_with_class.cpp
+++ udacity_practice/project2_htop_scratch/read_pid_info_with_class.cpp
@@ -1,13 +1,15 @@
 #include <dirent.h>
 #include <unistd.h>
 
-#include <algorithm>  // for all_of
+#include <algorithm> // for all_of
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
+
+#define hertz sysconf(_SC_CLK_TCK)
 
 using std::cout;
 using std::stof;
@@ -20,8 +22,8 @@ using std::vector;
 vector<int> Pids() {
   vector<int> pids;
   string procdir = "/proc";
-  DIR* directory = opendir(procdir.c_str());
-  struct dirent* file;
+  DIR *directory = opendir(procdir.c_str());
+  struct dirent *file;
   while ((file = readdir(directory)) != nullptr) {
     // Is this a directory?
     if (file->d_type == DT_DIR) {
@@ -56,10 +58,23 @@ unordered_map<int, string> Uid_User_Map() {
   return uid_user_map;
 }
 
+long SystemUpTime() {
+  long uptime;
+  string line;
+  string fileloc = "/proc/uptime";
+  std::ifstream stream(fileloc);
+  if (stream.is_open()) {
+    std::getline(stream, line);
+    std::istringstream linestream(line);
+    linestream >> uptime;
+  }
+  return uptime;
+}
+
 class Process {
- public:
+public:
   Process(int pid,
-          unordered_map<int, string> uid_user_map);  // check if this is allowed
+          unordered_map<int, string> uid_user_map); // check if this is allowed
 
   int Pid();
   string User();
@@ -67,20 +82,26 @@ class Process {
   float CpuUtilization();
   string Ram();
   long int UpTime();
+  void Cpu_Mem_Utime();
 
- private:
+private:
   int pid;
   string user;
   float cpu_util;
-  int ram;
+  string ram;
   long int uptime;
   string command;
+  unordered_map<int, string> uid_user_map;
 };
 
 Process::Process(int pid, unordered_map<int, string> uid_user_map)
-    : pid(pid), uid_user_map(uid_user_map){};
+    : pid(pid), uid_user_map(uid_user_map) {
+  Cpu_Mem_Utime();
+  User();
+  Command();
+};
 
-int Process::Pid(){return pid};
+int Process::Pid() { return pid; };
 
 string Process::User() {
   string status_file = "/proc/" + to_string(pid) + "/status";
@@ -93,46 +114,51 @@ string Process::User() {
       std::istringstream linestream(line);
       while (linestream >> stat_name >> an_id) {
         if (stat_name == "Uid:") {
-          user = uid_user_map[an_id]
+          user = uid_user_map[an_id];
           return user;
         }
       }
     }
   }
+  return " ";
 };
 
 string Process::Command() {
-  string user_file = "/proc/" + to_string(process_id) + "/cmdline";
+  string user_file = "/proc/" + to_string(pid) + "/cmdline";
   string line;
   std::ifstream filestream(user_file);
   if (filestream.is_open()) {
     while (std::getline(filestream, line)) {
       std::istringstream linestream(line);
+      command = line;
       return line;
     }
   }
   return " ";
 };
 
-float Process::CpuUtilization(){
-
+float Process::CpuUtilization() {
+  // Cpu_Mem_Utime();
+  return cpu_util;
 };
 
-string Process::Ram(){
-
+string Process::Ram() {
+  // Cpu_Mem_Utime();
+  return ram;
 };
 
-long int Process::UpTime(){
-
+long int Process::UpTime() {
+  // Cpu_Mem_Utime();
+  return uptime;
 };
 
-void Cpu_Mem_Utime(const int& process_id) {
+void Process::Cpu_Mem_Utime() {
   // https://man7.org/linux/man-pages/man5/proc.5.html
   // https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat/16736599#16736599
-  int hertz = sysconf(_SC_CLK_TCK); // convert to compiletime const
-  string user_file = "/proc/" + to_string(process_id) + "/stat";
-  string line;
-  int stat1, stat2, stat3, stat4, stat5, stat6, stat7, stat8, stat9, stat10,
+  // int hertz = sysconf(_SC_CLK_TCK); // convert to compiletime const
+  string user_file = "/proc/" + to_string(pid) + "/stat";
+  string line, stat2, stat3;
+  double stat1, stat4, stat5, stat6, stat7, stat8, stat9, stat10,
       stat11, stat12, stat13, stat14, stat15, stat16, stat17, stat18, stat19,
       stat20, stat21, stat22, stat23, stat24, stat25, stat26, stat27, stat28,
       stat29, stat30, stat31, stat32, stat33, stat34, stat35, stat36, stat37,
@@ -150,10 +176,42 @@ void Cpu_Mem_Utime(const int& process_id) {
           stat35 >> stat36 >> stat37 >> stat38 >> stat39 >> stat40 >> stat41 >>
           stat42 >> stat43 >> stat44 >> stat45 >> stat46 >> stat47 >> stat48 >>
           stat49 >> stat50 >> stat51 >> stat52;
-      uptime = stat14 + stat15 + stat16 + stat17; // also total_time
-      ram = stat23/1000;
-      seconds = system_uptime - (stat22 / hertz) // get system_uptime in seconds from /proc/uptime
-      cpu_util = 100 * ((uptime / hertz) / seconds)
+
+      uptime = stat14 + stat15 + stat16 + stat17;
+      int temp_ram = stat23 / 1000000;
+      ram = to_string(temp_ram) + " MB";
+      double temp_seconds = SystemUpTime() - (stat22 / hertz);
+      cpu_util = 100 * ((uptime / hertz) / temp_seconds);
     }
   }
+}
+
+int main() {
+  auto pids = Pids();
+  auto uid_user_map = Uid_User_Map();
+
+  vector<Process> processes;
+  for (auto pid : pids) {
+    Process temp_process(pid, uid_user_map);
+    processes.push_back(temp_process);
+  }
+
+  for (auto process : processes) {
+    cout << process.Pid() << "\t\t";
+    cout << process.User() << "\t\t";
+    cout << process.CpuUtilization() << "\t\t";
+    cout << process.Ram() << "\t\t";
+    cout << process.UpTime() << "\t\t";
+    string smallcmd = process.Command();
+    cout << smallcmd.substr(0, 30) << "\n";
+  }
+
+  // Process process(19088, uid_user_map);
+  // cout << process.Pid() << "\t\t";
+  // cout << process.User() << "\t\t";
+  // cout << process.CpuUtilization() << "\t\t";
+  // cout << process.Ram() << "\t\t";
+  // cout << process.UpTime() << "\t\t";
+  // string smallcmd = process.Command();
+  // cout << smallcmd.substr(0, 30) << "\n";
 }
