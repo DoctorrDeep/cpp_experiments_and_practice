ac5f2ebc18d4e18cf37b77bb4badf9ad597f318f
Ambar <ambar@calendar42.com>
2021-01-27 22:30:39 +0100

    templates, emplace_back instead of push_back, operator overloading attempt

--- udacity_practice/project2_htop_scratch/read_pid_info_with_class.cpp
+++ udacity_practice/project2_htop_scratch/read_pid_info_with_class.cpp
@@ -18,6 +18,39 @@ using std::to_string;
 using std::unordered_map;
 using std::vector;
 
+template <typename T> T getFileValue(string const &fileLoc){
+  T value;
+  string line;
+  std::ifstream filestream(fileLoc);
+  if (filestream.is_open()) {
+    std::getline(filestream, line);
+    std::istringstream linestream(line);
+    linestream >> value;
+  }
+  filestream.close();
+  return value;
+}
+
+template <typename T> T getFileValueByKey(string const &fileLoc, string const &myKey){
+  T value;
+  T temp_value;
+  string line;
+  string temp_key;
+  std::ifstream filestream(fileLoc);
+  if (filestream.is_open()) {
+    while (std::getline(filestream, line)) {
+      std::istringstream linestream(line);
+      while (linestream >> temp_key >> temp_value) {
+        if (temp_key == myKey) {
+          value = temp_value;
+        }
+      }
+    }
+  }
+  filestream.close();
+  return value;
+}
+
 // BONUS: Update this to use std::filesystem
 vector<int> Pids() {
   vector<int> pids;
@@ -31,7 +64,7 @@ vector<int> Pids() {
       string filename(file->d_name);
       if (std::all_of(filename.begin(), filename.end(), isdigit)) {
         int pid = stoi(filename);
-        pids.push_back(pid);
+        pids.emplace_back(pid);
       }
     }
   }
@@ -59,16 +92,7 @@ unordered_map<int, string> Uid_User_Map() {
 }
 
 long SystemUpTime() {
-  long uptime;
-  string line;
-  string fileloc = "/proc/uptime";
-  std::ifstream stream(fileloc);
-  if (stream.is_open()) {
-    std::getline(stream, line);
-    std::istringstream linestream(line);
-    linestream >> uptime;
-  }
-  return uptime;
+  return getFileValue<long>("/proc/uptime");
 }
 
 class Process {
@@ -83,7 +107,7 @@ public:
   string Ram();
   long int UpTime();
   void Cpu_Mem_Utime();
-  bool operator<(Process a) const ;
+  bool operator<(Process a) const;
 
 private:
   int pid;
@@ -106,22 +130,8 @@ int Process::Pid() { return pid; };
 
 string Process::User() {
   string status_file = "/proc/" + to_string(pid) + "/status";
-  string line;
-  string stat_name;
-  int an_id;
-  std::ifstream filestream(status_file);
-  if (filestream.is_open()) {
-    while (std::getline(filestream, line)) {
-      std::istringstream linestream(line);
-      while (linestream >> stat_name >> an_id) {
-        if (stat_name == "Uid:") {
-          user = uid_user_map[an_id];
-          return user;
-        }
-      }
-    }
-  }
-  return " ";
+  int uid = getFileValueByKey<int>(status_file, "Uid:");
+  return uid_user_map[uid];
 };
 
 string Process::Command() {
@@ -159,7 +169,7 @@ void Process::Cpu_Mem_Utime() {
   // int hertz = sysconf(_SC_CLK_TCK); // convert to compiletime const
   string user_file = "/proc/" + to_string(pid) + "/stat";
   string line, stat2, stat3;
-  double stat1, stat4, stat5, stat6, stat7, stat8, stat9, stat10, stat11,
+  long int stat1, stat4, stat5, stat6, stat7, stat8, stat9, stat10, stat11,
       stat12, stat13, stat14, stat15, stat16, stat17, stat18, stat19, stat20,
       stat21, stat22, stat23, stat24, stat25, stat26, stat27, stat28, stat29,
       stat30, stat31, stat32, stat33, stat34, stat35, stat36, stat37, stat38,
@@ -178,19 +188,27 @@ void Process::Cpu_Mem_Utime() {
           stat42 >> stat43 >> stat44 >> stat45 >> stat46 >> stat47 >> stat48 >>
           stat49 >> stat50 >> stat51 >> stat52;
 
-      uptime = stat14 + stat15 + stat16 + stat17;
-      int temp_ram = stat23 / 1000000;
-      ram = to_string(temp_ram) + " MB";
-      double temp_seconds = SystemUpTime() - (stat22 / hertz);
-      cpu_util = 100 * ((uptime / hertz) / temp_seconds);
+      // uptime = stat14 + stat15 + stat16 + stat17;
+      // int temp_ram = stat23 / 1000000;
+      // ram = to_string(temp_ram) + " MB";
+      // double temp_seconds = SystemUpTime() - (stat22 / hertz);
+      // cpu_util = 100 * ((uptime / hertz) / temp_seconds);
+
+      long int total_time = stat14 + stat15 + stat16 + stat17;
+      ram = to_string(stat23 / 1024);
+      uptime = SystemUpTime() - (stat22 / hertz);
+      cpu_util = (total_time / hertz) / (uptime + 1 * (uptime == 0));
+
+      cout << "\npid: " << pid << "\n";
+      cout << "stat22 : " << stat22 / hertz << "\n";
+      cout << "sys uptime : " << SystemUpTime() << "\n";
+      cout << "sys uptime - stat22 : " << SystemUpTime() - (stat22 / hertz)
+           << "\n";
     }
   }
 }
 
-
-bool Process::operator<(Process a) const {
-  return this->uptime < a.UpTime();
-}
+bool Process::operator<(Process a) const { return this->uptime < a.UpTime(); }
 
 int main() {
   auto pids = Pids();
@@ -199,7 +217,7 @@ int main() {
   vector<Process> processes;
   for (auto pid : pids) {
     Process temp_process(pid, uid_user_map);
-    processes.push_back(temp_process);
+    processes.emplace_back(temp_process);
   }
 
   // Test operator overloading to compare between 2 instances of process
@@ -216,14 +234,16 @@ int main() {
     cout << smallcmd.substr(0, 30) << "\n";
   }
 
-  // Process process(19088, uid_user_map);
+  // Process process(4, uid_user_map);
   // cout << process.Pid() << "\t\t";
   // cout << process.User() << "\t\t";
   // cout << process.CpuUtilization() << "\t\t";
   // cout << process.Ram() << "\t\t";
   // cout << process.UpTime() << "\t\t";
   // string smallcmd = process.Command();
+  // string smallc;
   // cout << smallcmd.substr(0, 30) << "\n";
+  // cout << smallc << "\n" << smallc.size() << "\n";
 
   // Process process1(7105, uid_user_map);
   // Process process2(7113, uid_user_map);
