269238f3e3432c784fe28dfeda04d886ef473620
Ambar <ambar@plotwise.com>
2021-02-04 17:46:42 +0100

    check time taken to fill array

--- udacity_practice/memory/fast_slow_array_access.cpp
+++ udacity_practice/memory/fast_slow_array_access.cpp
@@ -2,17 +2,17 @@
 #include <iostream>
 
 int main() {
-  const int size = 5;
+  const int size = 1000;
   static int x[size][size];
 
   // Code to save timestamp before array fillup
   auto t1 = std::chrono::high_resolution_clock::now();
 
-  for (int i=0; i < size; i++) {
-    for (int j=0; j < size; j++) {
-      x[j][i] = i + j;
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      // x[i][j] = i + j; // Good way to access arrays
+      x[j][i] = i + j; // Bad way to access arrays
       // std::cout << &x[j][i] << ": j = " << j << ": i = " << i << "\n";
-      std::cout << &x[j][i] << "\n";
     }
   }
 
