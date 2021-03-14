2337c86bab4ec43a1f0b266fdfa51ed237242f59
Ambar <ambar@calendar42.com>
2021-03-14 11:33:42 +0100

    identify constructor destructor, assignmnet operator overloading

--- udacity_practice/copy_practice_protection/exclusive_ownership_in_copies.cpp
+++ udacity_practice/copy_practice_protection/exclusive_ownership_in_copies.cpp
@@ -5,20 +5,27 @@ private:
   int *_myInt;
 
 public:
+  // Constructor
   ExclusiveCopy() {
     _myInt = (int *)malloc(sizeof(int));
     std::cout << "resource allocated" << std::endl;
   }
+
+  // destructor
   ~ExclusiveCopy() {
     if (_myInt != nullptr) {
       free(_myInt);
       std::cout << "resource freed" << std::endl;
     }
   }
+
+  // Copy constructor
   ExclusiveCopy(ExclusiveCopy &source) {
     _myInt = source._myInt;
     source._myInt = nullptr;
   }
+
+  // Copy assignment operator (operator overloading)
   ExclusiveCopy &operator=(ExclusiveCopy &source) {
     _myInt = source._myInt;
     source._myInt = nullptr;
