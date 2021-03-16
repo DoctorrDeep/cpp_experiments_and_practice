7ac03e5c4e83bfa46f34062c50a6cde9659686d6
Ambar <ambar@plotwise.com>
2021-03-16 11:12:02 +0100

    rvalue reference passed to function and comments

--- udacity_practice/lvalue_rvalue/r_value_references.cpp
+++ udacity_practice/lvalue_rvalue/r_value_references.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 
-void myFunction(int &val) { std::cout << "val = " << val << std::endl; }
-
+void myWorseFunction(int &val) { std::cout << "val = " << val << std::endl; }
+void myBetterFunction(int &&val) { std::cout << "val = " << val << std::endl; }
 int main() {
 
   // Case 1:
   int j = 42;
-  myFunction(j);
+  myWorseFunction(j); // works since j is an lvalue
 
-  myFunction(42); // blocks compilation. comment to run case 2
+  myWorseFunction(42); // blocks compilation. comment to run case 2
   // errors because 42 is an rvalue and the finction expected an lvalue
   // Output :
   // r_value_references.cpp:9:14: error: cannot bind non-const lvalue reference of type ‘int&’ to an rvalue of type ‘int’
-  //   9 |   myFunction(42);
-  //     |              ^~
+  //   9 |   myWorseFunction(42);
+  //     |                   ^~
 
   int k = 23;
-  myFunction(j + k); // blocks compilation. comment to run case 2
+  myWorseFunction(j + k); // blocks compilation. comment to run case 2
   // j, k are l values but the output of `j+k` is a temporary value, an r value!
   // r_value_references.cpp:16:16: error: cannot bind non-const lvalue reference of type ‘int&’ to an rvalue of type ‘int’
-  //  16 |   myFunction(j + k);
-  //     |              ~~^~~
+  //  16 |   myWorseFunction(j + k);
+  //     |                   ~~^~~
 
+  myBetterFunction(42); // this works because the function used `&&i` (rvalue reference)
 
   // Case 2:
   int a = 1; 
