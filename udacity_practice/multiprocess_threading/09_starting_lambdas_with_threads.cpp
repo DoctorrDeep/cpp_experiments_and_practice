d39a95afea58b981a4c4776e547d853e0608b2f3
ambar <ambar@source.ag>
2022-03-23 23:19:03 +0100

    doc: explan why one thread gets an updated value while the other  does not

--- udacity_practice/multiprocess_threading/09_starting_lambdas_with_threads.cpp
+++ udacity_practice/multiprocess_threading/09_starting_lambdas_with_threads.cpp
@@ -8,6 +8,8 @@ int main() {
   // Starting first thread by reference
   auto f0 = [&id]() {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    // By the time this wait is over, the value of id in the main thread will have incremented
+    // Hence the cout from the following statement will show value of id = 1
     std::cout << "a> id in 1st thread t1= " << id << std::endl;
   };
   std::thread t1(f0);
@@ -15,6 +17,9 @@ int main() {
   // Starting second thread by value
   std::thread t2([id]() {
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
+    // By the time this wait is over, the value of id in the main thread will have incremented
+    // BUT the value of id was set at the time the thread was created i.e. zero
+    // Hence the cout from the following statement will show value of id = 0
     std::cout << "b> id in 2nd thread t2= " << id << std::endl;
   });
 
